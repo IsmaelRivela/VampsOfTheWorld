621e704409d4a74f19fac745ad091f902e9c019b
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2024-06-12 17:23:04 +0200

    Github pages prueba

--- src/Servicios/ApiHandler.js
+++ src/Servicios/ApiHandler.js
@@ -1,5 +1,9 @@
 
 
-
+async function tattooFetch() {
+    const response = await fetch("http://example.com/movies.json");
+    const tattoos = await response.json();
+    console.log(tattoos);
+  }
 
 
