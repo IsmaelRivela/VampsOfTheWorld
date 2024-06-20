e25471aef7f24bdf1882d6947f4535d8fd324082
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2024-06-20 09:51:56 +0200

    changes

--- src/instagramAuth.jsx
+++ src/instagramAuth.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const InstagramAuth = () => {
   useEffect(() => {
     const clientId = '423180797362646'; // Reemplaza con tu app ID
-    const redirectUri = 'http://localhost:5174/auth'; // Reemplaza con tu redirect URI registrada
+    const redirectUri = 'https://vamps-of-the-world.vercel.app/'; // Reemplaza con tu redirect URI registrada
     const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
 
     window.location.href = authUrl; // Redirige al usuario a la URL de autorización de Instagram
