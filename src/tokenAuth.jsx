d9296920c019ecfbc4274f7bfd402b798ef8c272
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2024-06-20 20:50:16 +0200

    prueba

--- src/tokenAuth.jsx
+++ src/tokenAuth.jsx
@@ -2,49 +2,6 @@
 import React, { useEffect , useState } from 'react';
 import axios from 'axios';
 
-// const Auth = () => {
-//   useEffect(() => {
-//     const urlParams = new URLSearchParams(window.location.search);
-//     const code = urlParams.get('code');
-
-//     const getAccessToken = async (authorizationCode) => {
-//       const appId = '423180797362646'; // Reemplaza con tu app ID
-//       const appSecret = '90d000740688c3c34109edf7da140b29'; // Reemplaza con tu app secret
-//       const redirectUri = 'http://localhost:3000/auth'; // Debe coincidir con la redirect URI registrada
-
-//       const url = `https://api.instagram.com/oauth/access_token`;
-
-//       try {
-//         const response = await axios.post(url, {
-//           client_id: appId,
-//           client_secret: appSecret,
-//           grant_type: 'authorization_code',
-//           redirect_uri: redirectUri,
-//           code: authorizationCode,
-//         });
-
-//         console.log('Token de acceso:', response.data.access_token);
-
-//         localStorage.setItem('instagram_access_token', accessToken)
-//         // Guarda el token de acceso en tu estado o en algún almacenamiento seguro
-//       } catch (error) {
-//         console.error('Error al obtener el token de acceso:', error);
-//       }
-//     };
-
-//     if (code) {
-//       getAccessToken(code);
-//     }
-//   }, []);
-
-//   return (<div>Autenticación exitosa. Puedes cerrar esta ventana.</div>)
-// };
-
-// export default Auth;
-
-
-// src/components/Auth.js
-
 
 const Auth = () => {
 
@@ -89,7 +46,7 @@ const Auth = () => {
   return <div>{accessToken ? (
     <p>Token de acceso: {accessToken}</p>
   ) : (
-    <p>Esperando token de acceso...</p>
+    <p>Esperando token de acceso...2</p>
   )}
   <p>Autenticación exitosa. Puedes cerrar esta ventana.</p></div>;
 };
