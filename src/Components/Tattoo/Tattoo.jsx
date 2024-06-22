9f60f79982c69fb2e8256749abf9cffc61cb1ff4
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2024-06-22 23:31:11 +0200

    prueba

--- src/Components/Tattoo/Tattoo.jsx
+++ src/Components/Tattoo/Tattoo.jsx
@@ -4,15 +4,16 @@ const Tattoo = (props)=> {
 
     return(
 
-        <article className='Article'>
+        <a href='#' target='_blank'>
+            <article className='Article'>
 
-            <div className="Article-img-container">
-                <img className='Article-img'></img>
-            </div>
+                <div className="Article-img-container">
+                    <img className='Article-img'></img>
+                </div>
 
-            <h3 className='Article-h3'>{props.name}</h3>
-        </article>
-
+                <h3 className='Article-h3'>{props.name}</h3>
+            </article>
+        </a>
     )
 }
 
